619a156a72453ba5e1bf13afee3b008b9087435b
yong woo Yun <yongwooyun@hotmail.com>
2024-03-26 18:27:08 -0700

    change penalty time from 5 seconds to 10 seconds

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,9 +25,16 @@ const questions = [
 ];
 
 let currentQuestionIndex = 0;
+//set time to 60 seconds
 let timeLeft = 60;
 let timerInterval;
+//set value to 0 to start and laster use thes variable to caculate scores
+var correctAnswer = 0;
+var wrongAnswer = 0;
+var scoresPercentage = 0;
 
+
+//getElementById from HTML
 const quizStart = document.getElementById("quiz-start");
 const quizContainer = document.getElementById("quiz");
 const questionDisplay = document.getElementById("question");
@@ -39,14 +46,23 @@ const gameOverScreen = document.getElementById("game-over");
 const initialsForm = document.getElementById("initials-form");
 const highscoresList = document.getElementById("highscores");
 const highscoresLink = document.getElementById("highscores-link");
+var gameOverHeading = document.getElementById("game-over-heading");
+var quizTittle = document.getElementById("quizTittle");
+var quizDescription = document.getElementById("quizDescription");
 
+//function startQuiz
 function startQuiz() {
+   // quizTittle.textContent = "";
+   // quizDescription.textContent = "";
+
+   console.log (quizTittle.innerHTML)
+
     quizStart.classList.add("hidden");
     startTimer();
     showQuestion();
     quizContainer.classList.remove("hidden");
 }
-
+//time function
 function startTimer() {
     timerInterval = setInterval(() => {
         timeLeft--;
@@ -58,11 +74,13 @@ function startTimer() {
         }
     }, 1000);
 }
-// show contents  from questions
+// show contents  from questions function
 function showQuestion() {
+
     const currentQuestion = questions[currentQuestionIndex];
     questionDisplay.textContent = currentQuestion.question;
     answersDisplay.innerHTML = "";
+   
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
         button.textContent = answer;
@@ -74,12 +92,16 @@ function showQuestion() {
 function checkAnswer(answer) {
     const currentQuestion = questions[currentQuestionIndex];
     if (answer === currentQuestion.correctAnswer) {
+
         //if answer is correct display 'Correct Answer!" for one second
+        
         correctAnswerDisplay.classList.remove("hidden");
         setTimeout(() => {
             correctAnswerDisplay.classList.add("hidden");
         }, 1000);
-
+         //track counts of correct answers
+        correctAnswer += 1
+        // Move to the next question even if the answer is correct
         currentQuestionIndex++;
         if (currentQuestionIndex < questions.length) {
             showQuestion();
@@ -92,13 +114,15 @@ function checkAnswer(answer) {
         setTimeout(() => {
             wrongAnswerDisplay.classList.add("hidden");
         }, 1000);
+        //track counts of wrong answers
+        wrongAnswer += 1
 
-        //WHEN I answer a question incorrectly, then time is subtracted from the clock -5 seconds
+        //WHEN I answer a question incorrectly, then time is subtracted from the clock(timeLeft) -10 seconds
         console.log  (timeLeft) 
-        timeLeft = timeLeft - 5        
-
-
-        currentQuestionIndex++; // Move to the next question even if the answer is wrong
+        timeLeft = timeLeft - 10       
+         
+        // Move to the next question even if the answer is wrong
+        currentQuestionIndex++; 
         if (currentQuestionIndex < questions.length) {
             showQuestion();
         } else {
@@ -108,6 +132,23 @@ function checkAnswer(answer) {
 }
 //if user does not anwser the quiz with 60 second it goes to this or complete the quiz 
 function gameOver() {
+    //Game over message with score information 
+    var combinedScores = correctAnswer + wrongAnswer
+    scoresPercentage = correctAnswer/(correctAnswer+wrongAnswer) * 100
+    console.log ("scores: "+ scoresPercentage)
+    
+    
+    console.log(combinedScores)
+    //if Quiz question are not answered within 60 second Message
+    if (combinedScores !== 4 ){
+        gameOverHeading.textContent = "Time has passed, You faild the quiz, Please try again !"
+    //if Quiz is completed Message with scores    
+    } else{
+    gameOverHeading.textContent = gameOverHeading.textContent + " Correct Answers: " + correctAnswer + " Wrong Answers: " + wrongAnswer + " Your Grades for This Quiz: " + scoresPercentage + " %";
+    }
+    
+    console.log("correct:" + correctAnswer);
+    console.log("wrong:" + wrongAnswer);
     quizContainer.classList.add("hidden");
     gameOverScreen.classList.remove("hidden");
     clearInterval(timerInterval);
@@ -115,22 +156,26 @@ function gameOver() {
     highscoresLink.classList.remove("hidden");
 }
 
-//once, user submit the intitials, it saves to local storage with key 'Scores' with intial and score
+//once, user submit the intitials, it saves to local storage with key 'Scores' with intial and score info
 function saveHighscore(event) {
     event.preventDefault();
-    const initials = document.getElementById("initials").value.toUpperCase();
-    const score = timeLeft;
-    const highscoreData = JSON.parse(localStorage.getItem("Scores")) || [];
-    highscoreData.push({ initials, score });
-    highscoreData.sort((a, b) => b.score - a.score);
-    localStorage.setItem("Scores", JSON.stringify(highscoreData));
-    displayHighscores();
+    console.log ("scores: "+ scoresPercentage)
+
+
+
+   const initials = document.getElementById("initials").value.toUpperCase();
+   const score = scoresPercentage;
+   const highscoreData = JSON.parse(localStorage.getItem("Scores")) || [];
+   highscoreData.push({ initials, score });
+   highscoreData.sort((a, b) => b.score - a.score);
+   localStorage.setItem("Scores", JSON.stringify(highscoreData));
+   displayHighscores();
 }
 
 //displying all the data from local storage from key 'scores' and adding html elements <li> to highscoresList
 function displayHighscores() {
     const highscoreData = JSON.parse(localStorage.getItem("Scores")) || [];
-    highscoresList.innerHTML = highscoreData.map(data => `<li>${data.initials}: <span>${data.score}</span></li>`).join('');
+    highscoresList.innerHTML = highscoreData.map(data => `<li>${data.initials}: <span>${data.score} % </span></li>`).join('');
 }
 
     highscoresLink.addEventListener("click", () => {
